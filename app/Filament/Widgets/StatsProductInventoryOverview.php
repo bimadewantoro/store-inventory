9c4338272b1098f7d65cbf3891e26e073434a0ca
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-24 14:41:56 +0700

    feat: [BD] dashboard

---
+++ app/Filament/Widgets/StatsProductInventoryOverview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\ProductInventory;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsProductInventoryOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
