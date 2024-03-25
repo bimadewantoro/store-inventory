1ac3560ae6db7cafa7a6cc3ff9ff0c92bf074ba2
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-25 21:34:37 +0700

    feat: [BD] styling daily report

--- app/Filament/Resources/DailyReportResource.php
+++ app/Filament/Resources/DailyReportResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\DailyReportResource\Pages;
 use App\Filament\Resources\DailyReportResource\RelationManagers;
 use App\Models\ProductInventory;
+use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -18,9 +19,7 @@ class DailyReportResource extends Resource
 {
     protected static ?string $model = ProductInventory::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    protected static ?string $pluralModelLabel = 'Laporan Harian';
+    protected static ?string $navigationIcon = 'heroicon-o-document';
 
     protected static ?string $navigationLabel = 'Laporan Harian';
 
@@ -52,12 +51,16 @@ class DailyReportResource extends Resource
                     ->label('Keterangan')
                     ->getStateUsing(function (ProductInventory $record) {
                         return $record->inventory_type === 'in' ? 'Stok Masuk' : 'Stok Keluar';
-                    }),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->label('Ditambahkan Pada')
-                    ->searchable()
-                    ->date('d M Y H:i:s')
-                    ->sortable(),
+                    })
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'Stok Masuk' => 'success',
+                        'Stok Keluar' => 'danger',
+                    })
+                    ->icon(fn (string $state): string => match ($state) {
+                        'Stok Masuk' => 'heroicon-o-arrow-down-circle',
+                        'Stok Keluar' => 'heroicon-o-arrow-up-circle',
+                    })
             ])
             ->filters([
                 //
@@ -96,8 +99,10 @@ class DailyReportResource extends Resource
             ->whereDate('created_at', now()->format('Y-m-d'));
     }
 
-    public function getHeading(): string
+    public static function getPluralModelLabel(): string
     {
-        return __('Custom Page Heading');
+        $today = Carbon::now()->format('d M Y');
+
+        return "Laporan Harian - $today";
     }
 }
