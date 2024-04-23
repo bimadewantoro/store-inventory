d2acdbe1e28b7e5bd8c9c0abaf640f715a906a51
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-04-23 16:18:26 +0700

    fix: [BD] migration refresh

--- database/migrations/2024_03_31_121706_delete_column_from_products_table.php
+++ database/migrations/2024_03_31_121706_delete_column_from_products_table.php
@@ -22,9 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('product_code')->unique();
-            $table->string('description')->nullable();
-        });
+        //
     }
 };
